7a59cb66d4f7b651bb0560c4014dad0c4897f04d
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-04-02 21:20:33 -0300

    first commit

---
+++ src/main/java/com/api/fleche/FlecheApplication.java
@@ -0,0 +1,13 @@
+package com.api.fleche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlecheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/fleche/FlecheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fleche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlecheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
