b5db878add1410b5a6e928db65e8711c55df79dc
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-04-22 21:08:40 -0300

    feat: criada configuração de CORS para o front-end

--- src/main/java/com/api/fleche/infra/security/SecurityConfigurations.java
+++ src/main/java/com/api/fleche/infra/security/SecurityConfigurations.java
@@ -24,12 +24,12 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors().and()
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/bares").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/cadastrar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
